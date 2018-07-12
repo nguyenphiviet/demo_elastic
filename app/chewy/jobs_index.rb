e494161335175781f2008fe13e354c4b57b6785b
Nguyen Phi Viet <nguyen.phi.viet@framgia.com>
2018-07-12 10:06:21 +0700

    search multi fields

--- app/chewy/jobs_index.rb
+++ app/chewy/jobs_index.rb
@@ -1,4 +1,18 @@
 class JobsIndex < Chewy::Index
+  settings analysis: {
+    analyzer: {
+      my_ngram_analyzer: {
+        tokenizer: "my_ngram_tokenizer"
+      }
+    },
+    tokenizer: {
+      my_ngram_tokenizer: {
+          type: "nGram",
+          token_chars: ["letter", "digit", "whitespace", "punctuation", "symbol"]
+      }
+    }
+  }
+
   define_type Job.includes(:company) do
     field :name
     field :description
--- app/models/job.rb
+++ app/models/job.rb
@@ -8,4 +8,21 @@ class Job < ApplicationRecord
   update_index "companies#company" do
     previous_changes["company_id"] || company
   end
+
+  def self.search_elastic query
+    JobsIndex::Job.query(
+      multi_match: {
+        query: query,
+        type: "phrase_prefix",
+        fields: ["name", "description", "company.name", "company.address", "company.email"]
+      }
+    )
+  end
+  # type: "phrase_prefix",
+
+  def self.get_all
+    JobsIndex::Job.query(
+      match_all: {}
+    )
+  end
 end
